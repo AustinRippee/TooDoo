88ea6e3c1aa6692375bf4f2555945c9d0d099c74
Austin Rippee <austinrippee@gmail.com>
2025-07-10 22:09:12 -0400

    Added content

--- toodooapp/urls.py
+++ toodooapp/urls.py
@@ -17,6 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.contrib import admin
+from django.urls import path
+from todo import views
+
 urlpatterns = [
+    path('', views.index, name="todo"),
+    path('del/<str:item_id>', views.remove, name="del"),
     path('admin/', admin.site.urls),
 ]
